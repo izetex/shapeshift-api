412e0e38c3059cdc16334db4096eaf248b8c3b68
astudnev <aleksey@wondersoft.ru>
2017-12-08 13:04:20 +0300

    module self

--- lib/shapeshift/api.rb
+++ lib/shapeshift/api.rb
@@ -2,9 +2,9 @@ require "shapeshift/api/version"
 require 'net/http'
 
 module Shapeshift
-  class Api
+  module Api
 
-    def coins
+    def self.coins
       get 'getcoins'
     end
 
@@ -13,11 +13,11 @@ module Shapeshift
     # rate = from_symbol / to_symbol
     # minerFee - measured in to_symbol
     #
-    def rate from_symbol, to_symbol = 'btc'
+    def self.rate from_symbol, to_symbol = 'btc'
       get "marketinfo/#{pair(from_symbol, to_symbol)}"
     end
 
-    def quote amount_to, from_symbol, to_symbol = 'btc'
+    def self.quote amount_to, from_symbol, to_symbol = 'btc'
       post 'sendamount', {amount: amount_to, pair: pair(from_symbol, to_symbol)}
     end
 
@@ -27,29 +27,29 @@ module Shapeshift
     # "depositAmount"=>"0.02696698", "expiration"=>1512726449980, "quotedRate"=>"1557.46037437",
     # "maxLimit"=>11.57075248, "apiPubKey"=>"shapeshift", "minerFee"=>"12"}}
     #
-    def sendamount amount_to, address_to, from_symbol, to_symbol = 'btc'
+    def self.sendamount amount_to, address_to, from_symbol, to_symbol = 'btc'
       post 'sendamount', {amount: amount_to, withdrawal: address_to, pair: pair(from_symbol, to_symbol)}
     end
 
     # {"status"=>"no_deposits", "address"=>"0x553beec262e0ab11dfe43f7d60dc1f51f03e4398"}
-    def status address
+    def self.status address
       get "txStat/#{address}"
     end
 
 
     private
 
-    def pair from_symbol, to_symbol
+    def self.pair from_symbol, to_symbol
       "#{from_symbol.to_s.downcase}_#{to_symbol.to_s.downcase}"
     end
 
-    def get method_args
+    def self.get method_args
       r = Net::HTTP.get(URI.parse("https://shapeshift.io/#{method_args}"))
       JSON.parse(r)
     end
 
 
-    def post method, args
+    def self.post method, args
       r = Net::HTTP.post_form(URI.parse("https://shapeshift.io/#{method}"), args)
       JSON.parse(r.body)
     end
